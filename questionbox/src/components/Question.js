eb68e2a57a80ff61608f9145caa11c6b3bac9f93
roanblanchard <roan.g.blanchard@icloud.com>
2021-07-21 13:35:13 -0400

    expanded working

--- questionbox/src/components/Question.js
+++ questionbox/src/components/Question.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const Question = (props) => {
+    const [expanded, setExpanded] = useState(false)
     const { questions } = props
-    
-    return (
-        <>
-        {questions.map((question, idx) => {
-            return (
-                <div className="question-card">
-                <h2>{question.title}</h2>
-                <h3>{question.text}</h3>
-                <p>{question.username}</p>
-                <p>{question.created_at}</p>
-                </div>
-            )
-            
-    })}
-    </>
-    )
+
+    // const seeDetails = () => {
+    //     setExpanded(true)
+    // }
+
+    if (expanded) {
+        return (
+            <h1>I HAVE BEEN EXPANDED</h1>
+        )
+    } else {
+        return (
+            <>
+            {questions.map((question, idx) => {
+                return (
+                    <div className="question-card" onClick={() => setExpanded(true)}>
+                    <h2>{question.title}</h2>
+                    <p>{question.username}</p>
+                    <p>{question.created_at}</p>
+                    </div>
+                )
+                
+        })}
+        </>
+        )
+    }
 }
