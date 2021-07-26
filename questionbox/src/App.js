91f175e5885ec90fd5e221029f6df7eb2fc57084
roanblanchard <roan.g.blanchard@icloud.com>
2021-07-26 17:03:08 -0400

    added fake API call to question call

--- questionbox/src/App.js
+++ questionbox/src/App.js
@@ -1,141 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import useLocalStorageState from 'use-local-storage-state'
 import './App.css'
 import { Question } from './components/Question.js'
 import { QuestionDetails } from './components/QuestionDetails'
 import { Sidebar } from './components/Sidebar'
+import { Login } from './components/Login'
+import { requestQuestions } from './api'
 
 export function App () {
   const [expanded, setExpanded] = useState(false)
   const [selectedQuestion, setSelectedQuestion] = useState(null)
-  const [data, setData] = useState({
-    answers: [
-      {
-        id: '1',
-        text: 'stufffffff',
-        username: 'shaune',
-        approved: 'false',
-        favorited: 'false',
-        created_at: '0.00007 seconds ago'
-      },
-      {
-        id: '2',
-        text: 'whyd you do it like that',
-        username: 'roan',
-        approved: 'false',
-        favorited: 'false',
-        created_at: '3 days ago'
-      },
-      {
-        id: '3',
-        text: 'use this library instead duh',
-        username: 'dee',
-        approved: 'false',
-        favorited: 'false',
-        created_at: 'friday'
-      },
-      {
-        id: '4',
-        text: 'idk i dont use that',
-        username: 'emily',
-        approved: 'false',
-        favorited: 'false',
-        created_at: '54 years ago'
-      }
-    ],
-    questions: [
-      {
-        id: '1',
-        title: 'centering',
-        text: 'how do i center a div?',
-        username: 'roan',
-        created_at: '8 years ago',
-        favorited: 'false'
-      },
-      {
-        id: '2',
-        title: 'new here',
-        text: 'how do i post a question?',
-        username: 'emily',
-        created_at: 'tomorrow woah crazy',
-        favorited: 'false'
-      },
-      {
-        id: '3',
-        title: 'BSCode?',
-        text: 'which IDE should I use? I was thinking about the notes app.',
-        username: 'shaune',
-        created_at: 'today',
-        favorited: 'false'
-      },
-      {
-        id: '4',
-        title: 'IE4???',
-        text: 'how do I support Internet Explorer 4 in css?',
-        username: 'dee',
-        created_at: 'yesterday',
-        favorited: 'false'
-      },
-      {
-        id: '1',
-        title: 'centering',
-        text: 'how do i center a div?',
-        username: 'roan',
-        created_at: '8 years ago',
-        favorited: 'false'
-      },
-      {
-        id: '2',
-        title: 'new here',
-        text: 'how do i post a question?',
-        username: 'emily',
-        created_at: 'tomorrow woah crazy',
-        favorited: 'false'
-      },
-      {
-        id: '3',
-        title: 'BSCode?',
-        text: 'which IDE should I use? I was thinking about the notes app.',
-        username: 'shaune',
-        created_at: 'today',
-        favorited: 'false'
-      },
-      {
-        id: '4',
-        title: 'IE4???',
-        text: 'how do I support Internet Explorer 4 in css?',
-        username: 'dee',
-        created_at: 'yesterday',
-        favorited: 'false'
-      }
-    ]
+  const [data, setData] = useState([])
+  const [token, setToken] = useLocalStorageState('token', '')
+
+  function setAuthToken (token) {
+    setToken(token)
   }
-  )
 
-  return expanded
-    ? <>
-      <h1 className='title'>QuestionBox</h1>
-      <main>
-        <Sidebar />
-        <QuestionDetails question={selectedQuestion} setExpanded={setExpanded} />
-      </main>
-    </>
-    : (
-      <>
-        <h1 className='title'>QuestionBox</h1>
-        <main>
-          <Sidebar />
-          <div className='questions'>
-            <h2>Top Questions</h2>
-            {data.questions.map((question, idx) => {
-              return (
-                <Question question={question} setExpanded={setExpanded} setSelectedQuestion={setSelectedQuestion} />
-              )
-            })}
-          </div>
+  useEffect(() => {
 
-        </main>
-      </>
-      )
+    if (token) {
+        requestQuestions(token)
+    // this promise will extract the data from
+    // the api response
+      .then(data => {
+        setData(data)
+      })
+    }
+      // re-render the component if the username
+      // or token changes
+  }, [token])
+
+  console.log(data)
+
+    if (token) { 
+        return expanded
+            ? <>
+            <h1 className='title'>QuestionBox</h1>
+            <main>
+                <Sidebar />
+                <QuestionDetails question={selectedQuestion} setExpanded={setExpanded} />
+            </main>
+            </>
+            : (
+            <>
+                <h1 className='title'>QuestionBox</h1>
+                <main>
+                    <Sidebar />
+                    <div className='questions'>
+                        <h2>Top Questions</h2>
+                        {data.map((question, idx) => {
+                        return (
+                            <Question question={question} setExpanded={setExpanded} setSelectedQuestion={setSelectedQuestion} />
+                        )
+                        })}
+                    </div>
+                </main>
+            </>
+        )
+        } else {
+            return (
+                <Login setAuthToken={setAuthToken} />
+            )
+        }
 }
 
 export default App
---
+++ questionbox/src/components/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+import { requestLogin } from '../api.js'
+
+export const Login = ({ setAuthToken }) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState('')
+  
+    function handleSubmit (e) {
+      e.preventDefault()
+      // here I am making a fake api call to
+      // authenticate my user
+      requestLogin(username, password)
+        .then((data) => {
+          if (data && data.auth_token) {
+            setAuthToken(data.auth_token)
+          }
+        })
+        .catch((error) => {
+          setErrors(error.message)
+        })
+    }
+  
+    return (
+      <>
+        {errors &&
+          <p>{errors}</p>}
+        <form className='form' onSubmit={handleSubmit}>
+          <label className='label'>Username</label>
+          <input
+            className='input'
+            type='text'
+            placeholder='email@domain.com or janedoge123'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label className='label'>Password</label>
+          <input
+            className='input'
+            type='password'
+            // using state to pass a value to this attribute
+            // makes this a controlled component
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div id='form-buttons'>
+            <button
+              className=''
+              type='submit'
+            >Login
+            </button>
+            <span>New to QuestionBox? &nbsp; <a className=''>Register Now</a></span>
+          </div>
+        </form>
+      </>
+    )
+  }
+  
+  export default Login
