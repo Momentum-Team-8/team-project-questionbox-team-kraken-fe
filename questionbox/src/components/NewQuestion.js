8c37a2103d5f9ea5f1e4644f171f187b2e4f5eae
roanblanchard <roan.g.blanchard@icloud.com>
2021-07-26 20:33:59 -0400

    added new question form and button to home screen

---
+++ questionbox/src/components/NewQuestion.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+export const NewQuestion = () => {
+    const [text, setText] = useState('')
+    const [title, setTitle] = useState('')
+    const handleChange = (event) => {
+    setText(event.target.value)
+    }
+    const handleTitle = (event) => {
+        setTitle(event.target.value)
+    }
+  return (
+    <>
+      <form className="new-question-form">
+        <h3>Ask a question...</h3>
+        <div className="question-title">
+            <label for="title">Title</label>
+            <textarea
+                id="title"
+                value={title}
+                onChange={handleTitle}
+            />
+        </div>
+        <div className="question-text">
+            <label for="body">Body</label>
+            <textarea
+                placeholder="remember, be nice..."
+                id="body"
+                value={text}
+                onChange={handleChange}
+            />
+        </div>
+        <div>
+          <input className="submit" type='submit' value='Submit' />
+        </div>
+      </form>
+    </>
+  )
+}
